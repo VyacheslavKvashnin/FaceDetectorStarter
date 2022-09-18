8d0fb61db79313bb93de2a53dfeb20b7ba135e99
komplektan <komplektan@mail.ru>
2022-09-18 15:39:24 +0500

    Start project

--- Detector/ViewController.swift
+++ Detector/ViewController.swift
@@ -7,19 +7,46 @@
 //
 
 import UIKit
+import CoreImage
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var personPic: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        personPic.image = UIImage(named: "face-1")
+
     }
+    
+    func detect() {
+        
+        guard let personciImage = CIImage(image: personPic.image!) else {
+            return
+        }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        let accuracy = [CIDetectorAccuracy: CIDetectorAccuracyHigh]
+        let faceDetector = CIDetector(ofType: CIDetectorTypeFace, context: nil, options: accuracy)
+        let faces = faceDetector!.features(in: personciImage)
+        
+        for face in faces as! [CIFaceFeature] {
+            
+            print("Found bounds are \(face.bounds)")
+            
+            let faceBox = UIView(frame: face.bounds)
+
+            faceBox.layer.borderWidth = 3
+            faceBox.layer.borderColor = UIColor.red.cgColor
+            faceBox.backgroundColor = UIColor.clear
+            personPic.addSubview(faceBox)
+            
+            if face.hasLeftEyePosition {
+                print("Left eye bounds are \(face.leftEyePosition)")
+            }
+            
+            if face.hasRightEyePosition {
+                print("Right eye bounds are \(face.rightEyePosition)")
+            }
+        }
     }
-
-
 }
-
